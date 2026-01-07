981d90ddc35e915b071b72c0b052e9a9d0413a12
Matfen <matfen3.05@gmail.com>
2026-01-07 10:59:02 +0100

    feat: Ajouter CI Pipeline

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("Salut tout le monde");
+console.log("Je suis un apprenti DevOps");
+console.log("Je crée mon tout premier pipeline CI");
+
+
---
+++ test.js
@@ -0,0 +1,4 @@
+console.log("Début du test");
+setTimeout(() => console.log("Attendre 3 secondes"), 3000);
+console.log("Fin du test");
+
